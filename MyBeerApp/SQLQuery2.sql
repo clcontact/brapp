f3ec268f062867b8e672e95f52c02e24e7fb2c36
clcontact <clcontact@gmail.com>
2013-02-22 20:46:28 -0500

    Trying to get NuGet not to fail

---
+++ MyBeerApp/SQLQuery2.sql
@@ -0,0 +1,11 @@
+﻿declare @TableName varchar(100) = 'Country'
+SELECT 'Insert into ' so.name, sc.name, st.name, sc.length
+, Case when sc.status = 0x80 then 'Y' else 'N' END as IsIdent
+, ColOrder
+FROM sysobjects so
+INNER JOIN syscolumns sc
+  ON so.id= sc.id 
+INNER JOIN systypes st
+  ON sc.xtype = st.xusertype 
+WHERE so.Name = @TableName
+ORDER BY ColOrder
